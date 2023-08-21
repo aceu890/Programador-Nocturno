024e0868febfdc9350af610743e9790ae5c6e17b
Fernando <eldersoto1876@hotmail.com>
2023-08-20 22:18:16 -0400

    finish modal - working login admin

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,23 +1,37 @@
-// TODO: SECRETO DEL LOGO 1
-const secretDiv = document.getElementById('logo-secret');
-let clickCount = 0;
+// TODO: MODAL LOGIN
+const openModal = document.querySelector('.hero_cta');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.modal_close');
 
-secretDiv.addEventListener('click', () => {
-    clickCount++;
-    
-    if (clickCount === 10) {
-        arrowRight();
-        clickCount = 0;
-    }
+openModal.addEventListener('click', (e) => {
+    e.preventDefault();
+    modal.classList.add('modal--show');
 });
 
-function arrowRight() {
-    const logo = document.querySelector('.logo');
-    logo.src = './assets/img/login.svg'; // Cambia la ruta de la nueva imagen
-    logo.style.width = '30px';
-}
+// closeModal.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     modal.classList.remove('modal--show');
+// });
 
 /* ===================================================
    ===================================================
    =================================================== */
 
+// TODO: LOGIN
+
+const power = document.getElementById('power');
+
+power.addEventListener('click', function () {
+    const loginForm = document.getElementById("login-form");
+
+        const user = loginForm.user.value;
+        const password = loginForm.password.value;
+
+        if (user === "admin" && password === "admin") {
+            alert("Acceso aprobado")
+    
+        } else {
+            alert("Error al autenticarse")
+        }
+   
+});
